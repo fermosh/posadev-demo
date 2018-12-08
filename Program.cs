37ca14ed9c4255cb7a345d16efa745986b7fa915
Oscar Legorreta <you@example.com>
2018-12-08 00:50:05 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace posadev_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello from {Environment.OSVersion.ToString()}!");
+        }
+    }
+}
